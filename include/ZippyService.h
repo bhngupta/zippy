5cd625aef0f3d222d0b132c2ee0e384a1596b5a6
Purvav Punyani <purvavsocial@gmail.com>
2024-06-12 23:18:25 -0400

    chore: added concurrency to the server

--- include/ZippyService.h
+++ include/ZippyService.h
@@ -3,16 +3,27 @@
 
 #include "zippy.grpc.pb.h"
 #include "Database.h"
+#include <grpcpp/grpcpp.h>
+#include <thread>
+#include <vector>
 
-class ZippyService final : public zippy::Zippy::Service {
+class ZippyService final : public zippy::Zippy::AsyncService {
 public:
-    explicit ZippyService(Database& db) : db_(db) {}
+    explicit ZippyService(Database& db);
+    ~ZippyService();
 
     grpc::Status ExecuteCommand(grpc::ServerContext* context, const zippy::CommandRequest* request, zippy::CommandResponse* response) override;
 
+    void HandleRpcs();
+
+    void log(const std::string& operation, const std::string& key, const std::string& value = "");
+
+    std::vector<std::thread> threads_;
+    std::unique_ptr<grpc::ServerCompletionQueue> cq_;
+    std::unique_ptr<grpc::Server> server_;
+
 private:
     Database& db_;
-    void log(const std::string& operation, const std::string& key, const std::string& value = "");
 };
 
 #endif // ZIPPYSERVICEIMPL_H
